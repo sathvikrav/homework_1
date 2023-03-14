69543a6cba4efba2591fa188698b0b899d15c0e9
Sathvik Ravi <sathvikravi@Sathviks-MacBook-Air.local>
2023-03-14 14:35:23 -0400

    adding scan files

--- scan/scan.h
+++ scan/scan.h
@@ -23,7 +23,42 @@ T scan_inplace_serial(T *A, size_t n, const F& f, T id) {
 // The work/depth bounds of your implementation should be:
 // Work = O(n)
 // Depth = O(\log(n))
+
 template <class T, class F>
 T scan_inplace(T *A, size_t n, const F& f, T id) {
-  return id;  // TODO
+  // return id;  // TODO
+  // return id;
+  T* L = (T*)malloc((n - 1) * sizeof(T));
+  T total = scan_up(A, n, L, n - 1, f);
+  scan_down(A, n, L, n - 1, f, id);
+  return total;
+}
+
+template <class T, class F>
+size_t scan_up(T *A, size_t n, T *L, size_t k, const F& f) {
+  if (n == 1) {
+    return A[0];
+  } else {
+    size_t m = n / 2;
+    T l, r;
+    auto f1 = [&]() { l = scan_up(A, m, L, m - 1, f); };
+    auto f2 = [&]() { r = scan_up(A + m, n, L + m, k, f); };
+    par_do(f1, f2);
+    L[m - 1] = l;
+    return f(l, r);
+  }
+}
+
+template <class T, class F>
+void scan_down(T *A, size_t n, T *L, size_t k, const F& f, T id) {
+  if (n == 1) {
+    A[0] = id;
+    return;
+  } else {
+    size_t m = n / 2;
+    auto f1 = [&]() { scan_down(A, m, L, m - 1, f, id); };
+    auto f2 = [&]() { scan_down(A + m, n, L + m, k, f, f(id, L[m - 1])); };
+    par_do(f1, f2);
+    return;
+  }
 }
